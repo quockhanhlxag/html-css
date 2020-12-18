add58ade05e9c02351e7b886f82c249dfc2582bf
DESKTOP-FIRK1OT\DELL <nqkhanhlxag@gmail.com>
2020-12-18 15:29:36 +0700

    update main.js

--- dento/js/main.js
+++ dento/js/main.js
@@ -1,9 +1,9 @@
-function addStickyBar(){
+function addStickyBar() {
   window.addEventListener("scroll", function () {
     var scrollY = window.scrollY;
     // console.log(scrollY)
     var navbar = $("#nav-bar")
-    
+
     // console.log('navbar', navbar)
     if (scrollY >= 100) {
       navbar.addClass("sticky-navbar");
@@ -14,36 +14,39 @@ function addStickyBar(){
 }
 
 function addRotateHambar() {
-  document.getElementById("hambar").addEventListener("click", function(){
+  document.getElementById("hambar").addEventListener("click", function () {
     var hambar = $("#hambar")
     hambar.toggleClass("active")
   })
 }
 
 function displaySidebar() {
-  document.getElementById("hambar").addEventListener("click", function(){
+  document.getElementById("hambar").addEventListener("click", function () {
     var sidebar = $("#sidebar")
     sidebar.toggleClass("menu-on")
   })
 }
 
-$('.counter').each(function () {
-  $(this).prop('Counter',0).animate({
+function numberCounter() {
+  $('.counter').each(function () {
+    $(this).prop('Counter', 0).animate({
       Counter: $(this).text()
-  }, {
-    
-    //change count up speed here
+    }, {
+
+      //change count up speed here
       duration: 4000,
       easing: 'swing',
       step: function (now) {
-          $(this).text(Math.ceil(now));
+        $(this).text(Math.ceil(now));
       }
+    });
   });
-});
+}
 
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
   addStickyBar()
   addRotateHambar()
   displaySidebar()
+  numberCounter()
 })
