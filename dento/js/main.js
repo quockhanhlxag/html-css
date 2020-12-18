dab86af361a94ea4521ee7bc4439f15e58c7d340
DESKTOP-FIRK1OT\DELL <nqkhanhlxag@gmail.com>
2020-12-18 15:20:53 +0700

    Add service block to Dento

--- dento/js/main.js
+++ dento/js/main.js
@@ -1,9 +1,9 @@
-function addStickyBar(){
+function addStickyBar() {
   window.addEventListener("scroll", function () {
     var scrollY = window.scrollY;
     // console.log(scrollY)
     var navbar = $("#nav-bar")
-    
+
     // console.log('navbar', navbar)
     if (scrollY >= 100) {
       navbar.addClass("sticky-navbar");
@@ -14,16 +14,41 @@ function addStickyBar(){
 }
 
 function addRotateHambar() {
-  document.getElementById("hambar").addEventListener("click", function(){
+  document.getElementById("hambar").addEventListener("click", function () {
     var hambar = $("#hambar")
     hambar.toggleClass("active")
   })
 }
 
 function displaySidebar() {
-  document.getElementById("hambar").addEventListener("click", function(){
+  document.getElementById("hambar").addEventListener("click", function () {
     var sidebar = $("#sidebar")
     sidebar.toggleClass("menu-on")
+  })
+}
+
+function playVideo() {
+  // Gets the video src from the data-src on each button
+
+  var $videoSrc;
+  $('.video-btn').click(function () {
+    $videoSrc = $(this).data("src");
+  });
+  console.log($videoSrc);
+
+  // when the modal is opened autoplay it  
+  $('#myModal').on('shown.bs.modal', function (e) {
+
+
+    // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+    $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+  })
+
+
+  // stop playing the youtube video when I close the modal
+  $('#myModal').on('hide.bs.modal', function (e) {
+    // a poor man's stop video
+    $("#video").attr('src', $videoSrc);
   })
 }
 
@@ -33,4 +58,5 @@ $(function () {
   addStickyBar()
   addRotateHambar()
   displaySidebar()
+  playVideo()
 })
